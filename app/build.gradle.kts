e988ebb84ab3f27130c83f0e2e49eb58ddd8fa09
Paul Obunga <paulobunga.one@gmail.com>
2026-08-02 18:12:12 +0300

    fix: upgrade AGP to 8.9.1 and compile/target SDK to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
   namespace = "com.litesails.saccomanager"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.litesails.saccomanager"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
